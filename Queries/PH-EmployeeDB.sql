9603b9c29d800e2763616d2bc349b2184633b507
dseg27 <disegovia4@gmail.com>
2021-11-08 17:48:32 -0700

    Added queries to database for challenge.

--- Queries/PH-EmployeeDB.sql
+++ Queries/PH-EmployeeDB.sql
@@ -228,6 +228,37 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- CHALLENGE 7 -- 
+-- DELIVERABLE 2 --
+
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees; 
+
+SELECT from_date, to_date
+FROM dept_emp;
+
+SELECT title 
+FROM titles;
+
+-- Create table 
+SELECT DISTINCT ON (e.emp_no) de.to_date,
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	ti.title
+-- INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON(e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON(e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, de.to_date DESC;
+	
 	
 
 
